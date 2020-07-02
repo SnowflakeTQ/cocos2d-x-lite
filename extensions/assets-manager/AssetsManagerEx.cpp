f23e8ccaab69533cf0edeac8bf63f526ff4f0253
gaoyang <geekgaoyang@gmail.com>
2020-07-02 17:05:58 +0800

    修复 Bug

--- extensions/assets-manager/AssetsManagerEx.cpp
+++ extensions/assets-manager/AssetsManagerEx.cpp
@@ -711,12 +711,12 @@ void AssetsManagerEx::downloadVersion()
 
     if (_targetVersion != -1) {
         std::string pattern = "version.manifest";
-        versionUrl = versionUrl.replace(versionUrl.find(pattern), sizeof(pattern) - 1, "build_" + std::to_string(_targetVersion) + "/res/project.manifest");
+        versionUrl = versionUrl.replace(versionUrl.find(pattern), pattern.length(), "build_" + std::to_string(_targetVersion) + "/res/project.manifest");
     }
     
     if (_isTestDevice) {
         std::string pattern = "version.manifest";
-        versionUrl = versionUrl.replace(versionUrl.find(pattern), sizeof(pattern) - 1, "test_version.manifest");
+        versionUrl = versionUrl.replace(versionUrl.find(pattern), pattern.length(), "test_version.manifest");
     }
     
     CCLOG("AssetsManagerEx : versionUrl %s\n", versionUrl.c_str());
