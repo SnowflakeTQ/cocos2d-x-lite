c16c425d916dd9b8312229c4580ffd20ed0290e5
闫继发 <yanjifa@joycastle.mobi>
2020-09-14 11:13:05 +0800

    :bug: 修复 meshbuffer bug

--- cocos/renderer/scene/MeshBuffer.cpp
+++ cocos/renderer/scene/MeshBuffer.cpp
@@ -2,17 +2,17 @@
  Copyright (c) 2018 Xiamen Yaji Software Co., Ltd.
 
  http://www.cocos2d-x.org
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,7 +28,7 @@
 #include "RenderFlow.hpp"
 #include "../gfx/DeviceGraphics.h"
 
-#define MAX_VB_SIZE 1310700
+#define MAX_VERTEX_COUNT 65535
 
 RENDERER_BEGIN
 
@@ -37,17 +37,17 @@ MeshBuffer::MeshBuffer(ModelBatcher* batcher, VertexFormat* fmt)
 , _batcher(batcher)
 {
     _bytesPerVertex = _vertexFmt->getBytes();
-    
+
     DeviceGraphics* device = _batcher->getFlow()->getDevice();
     _vb = VertexBuffer::create(device, _vertexFmt, Usage::DYNAMIC, nullptr, 0, 0);
     _vbArr.pushBack(_vb);
-    
+
     _ib = IndexBuffer::create(device, IndexFormat::UINT16, Usage::STATIC, nullptr, 0, 0);
-    _ib->retain();
-    
+    _ibArr.pushBack(_ib);
+
     _vDataCount = MeshBuffer::INIT_VERTEX_COUNT * 4 * _bytesPerVertex / sizeof(float);
     _iDataCount = MeshBuffer::INIT_VERTEX_COUNT * 6;
-    
+
     reallocVBuffer();
     reallocIBuffer();
 }
@@ -59,16 +59,19 @@ MeshBuffer::~MeshBuffer()
          _vbArr.at(i)->destroy();
     }
     _vbArr.clear();
-    
-    _ib->destroy();
-    _ib->release();
-    
+
+    for (std::size_t i = 0, n = _ibArr.size(); i < n; i++)
+    {
+         _ibArr.at(i)->destroy();
+    }
+    _ibArr.clear();
+
     if (iData)
     {
         delete[] iData;
         iData = nullptr;
     }
-    
+
     if (vData)
     {
         delete[] vData;
@@ -117,70 +120,37 @@ const MeshBuffer::OffsetInfo& MeshBuffer::request(uint32_t vertexCount, uint32_t
 
 const MeshBuffer::OffsetInfo& MeshBuffer::requestStatic(uint32_t vertexCount, uint32_t indexCount)
 {
+    checkAndSwitchBuffer(vertexCount);
     uint32_t byteOffset = _byteOffset + vertexCount * _bytesPerVertex;
-    if (MAX_VB_SIZE < byteOffset)
-    {
-        // Finish pre data.
-        _batcher->flush();
-        _vb->update(0, vData, _byteOffset);
-        
-        // Prepare next data.
-        _vbPos++;
-        if (_vbPos >= _vbArr.size())
-        {
-            DeviceGraphics* device = _batcher->getFlow()->getDevice();
-            _vb = VertexBuffer::create(device, _vertexFmt, Usage::DYNAMIC, nullptr, 0, 0);
-            _vb->setBytes(_vDataCount * VDATA_BYTE);
-            _vbArr.pushBack(_vb);
-        }
-        else
-        {
-            _vb = _vbArr.at(_vbPos);
-        }
-        
-        _byteStart = 0;
-        _byteOffset = 0;
-        _vertexStart = 0;
-        _vertexOffset = 0;
-        
-        _offsetInfo.vByte = 0;
-        _offsetInfo.vertex = 0;
-        
-        byteOffset = vertexCount * _bytesPerVertex;
-    }
-    
     uint32_t indexOffset = _indexOffset + indexCount;
     uint32_t vBytes = _vDataCount * VDATA_BYTE;
-    
+
     if (byteOffset > vBytes)
     {
         _oldVDataCount = _vDataCount;
-        
+
         while (vBytes < byteOffset)
         {
             _vDataCount *= 2;
             vBytes = _vDataCount * VDATA_BYTE;
         }
-        
+
         reallocVBuffer();
     }
-    
+
     if (indexOffset > _iDataCount)
     {
         _oldIDataCount = _iDataCount;
-        
+
         while (_iDataCount < indexOffset)
         {
             _iDataCount *= 2;
         }
-        
+
         reallocIBuffer();
     }
-    
-    _vertexOffset += vertexCount;
-    _indexOffset += indexCount;
-    _byteOffset = byteOffset;
-    _dirty = true;
+    updateOffset(vertexCount, indexCount, byteOffset);
+
     return _offsetInfo;
 }
 
@@ -191,10 +161,62 @@ void MeshBuffer::uploadData()
     _dirty = false;
 }
 
+void MeshBuffer::switchBuffer(uint32_t vertexCount)
+{
+    std::size_t offset = ++_vbPos;
+
+    _byteOffset = 0;
+    _vertexOffset = 0;
+    _indexOffset = 0;
+    _indexStart = 0;
+
+    if (offset < _vbArr.size())
+    {
+        _vb = _vbArr.at(offset);
+        _ib = _ibArr.at(offset);
+    }
+    else
+    {
+        DeviceGraphics* device = _batcher->getFlow()->getDevice();
+        _vb = VertexBuffer::create(device, _vertexFmt, Usage::DYNAMIC, nullptr, 0, 0);
+        _vb->setBytes(_vDataCount * VDATA_BYTE);
+        _vbArr.pushBack(_vb);
+
+        _ib = IndexBuffer::create(device, IndexFormat::UINT16, Usage::STATIC, nullptr, 0, 0);
+        _ib->setBytes(_iDataCount * IDATA_BYTE);
+        _ibArr.pushBack(_ib);
+    }
+}
+
+void MeshBuffer::checkAndSwitchBuffer(uint32_t vertexCount)
+{
+    if (_vertexOffset + vertexCount > MAX_VERTEX_COUNT)
+    {
+        uploadData();
+        _batcher->flush();
+        switchBuffer(vertexCount);
+    }
+}
+
+void MeshBuffer::updateOffset (uint32_t vertexCount, uint32_t indiceCount, uint32_t byteOffset)
+{
+    _offsetInfo.vertex = _vertexOffset;
+    _vertexOffset += vertexCount;
+
+    _offsetInfo.index = _indexOffset;
+    _indexOffset += indiceCount;
+
+    _offsetInfo.vByte = _byteOffset;
+    _byteOffset = byteOffset;
+
+    _dirty = true;
+}
+
 void MeshBuffer::reset()
 {
     _vbPos = 0;
     _vb = _vbArr.at(0);
+    _ib = _ibArr.at(0);
     _byteStart = 0;
     _byteOffset = 0;
     _vertexStart = 0;
